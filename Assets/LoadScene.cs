8d73da233b718263c641ee8afca37184315feb91
swangboy55 <mayansymbol@gmail.com>
2016-11-20 18:47:03 -0500

    holy guacamole
    updated game 2 scenes now

---
+++ Assets/LoadScene.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class LoadScene : MonoBehaviour
+{
+
+	// Use this for initialization
+	void Start ()
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	
+	}
+
+    public void SceneLoad(string scene)
+    {
+        SceneManager.LoadScene(scene);
+    }
+}
