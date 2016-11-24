c44b8e7a0b28d6498eb6efa769717dd5d676b01b
swangboy55 <mayansymbol@gmail.com>
2016-11-23 20:09:11 -0500

    player input

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	void FixedUpate()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+
+    }
+}
