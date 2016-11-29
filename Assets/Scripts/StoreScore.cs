abbbe13fc1dcac4587e694455d3b5e0f407e16f8
swangboy55 <mayansymbol@gmail.com>
2016-11-29 16:20:02 -0500

    Updated storescore

--- Assets/Scripts/StoreScore.cs
+++ Assets/Scripts/StoreScore.cs
@@ -20,38 +20,39 @@ public class StoreScore : MonoBehaviour {
 
     void Store(string name, int score)
     {
+        string entry = name + " : " + score;
         string[] lines = new string[20];
         if (!System.IO.File.Exists("scoreboard.txt"))
         {
             string[] Filllines = new string[20];
             for (int i = 0; i < Filllines.Length; i++)
             {
-                lines[i] = " : ";
+                Filllines[i] = " : ";
             }
+            Filllines[0] = entry;
             System.IO.File.WriteAllLines("scoreboard.txt", Filllines);
         }
         else
         {
             lines = System.IO.File.ReadAllLines("scoreboard.txt");
-            string entry = name + " : " + score;
             int i;
-            for (i =0; i < lines.Length; i++)
+            for (i = 0; i < lines.Length; i++)
             {
-                if(lines[i] == " : " )
+                if (lines[i] == " : ")
                 {
                     break;
                 }
-                if(score > int.Parse((lines[i].Substring(lines[i].IndexOf(':') + 2))))
+                if (score > int.Parse((lines[i].Substring(lines[i].IndexOf(':') + 2))))
                 {
                     break;
                 }
             }
 
-            for (int a=i; a<19; a++)
+            for (int a = 19; a > i; a--)
             {
-                lines[a + 1] = lines[a];                
+                lines[a] = lines[a - 1];
             }
-            if(i < 20)
+            if (i < 20)
             {
                 lines[i] = entry;
             }
@@ -61,7 +62,8 @@ public class StoreScore : MonoBehaviour {
 
         }
 
-       
-    }
 
+    }
 }
+
+//tutorail
