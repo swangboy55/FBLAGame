6b707b2d0e4d394d46f56f3df933363016ffc3d8
swangboy55 <mayansymbol@gmail.com>
2016-11-30 21:49:21 -0500

    wrote gamehandler partially :+1:
    it's pretty good

--- Assets/Scripts/GameHandler.cs
+++ Assets/Scripts/GameHandler.cs
@@ -1,15 +1,23 @@
 ﻿using UnityEngine;
+using UnityEngine.SceneManagement;
 using System.Collections;
 
 public class GameHandler : MonoBehaviour {
 
     public float MinSpeed;
     public float MaxSpeed;
+    public float PreparationSeconds = 10.0f;
+    public float timeAllowedUnderReq = 5f;
+    public string nextScene;
 
     public float SpeedInterval;
     public float SpeedIntervalRate = 1.0f;
 
     private float score;
+    private float timeStart;
+    private float velocityNeeded;
+    private float timeUnderReq = 0;
+    private float deltaTimeSum = 0;
 
     public float GetScore()
     {
@@ -17,12 +25,48 @@ public class GameHandler : MonoBehaviour {
     }
 
 	// Use this for initialization
-	void Start () {
-	    
+	void Start ()
+    {
+        timeStart = Time.time;
+        velocityNeeded = MinSpeed;
 	}
 	
 	// Update is called once per frame
-	void Update () {
-	
+	void Update ()
+    {
+        Debug.Log(GetComponent<Rigidbody2D>().velocity.magnitude + " , " + velocityNeeded + " , " + timeUnderReq );
+        if (Time.time < timeStart + PreparationSeconds)
+        {
+
+        }
+        else
+        {
+            deltaTimeSum += Time.deltaTime;
+            if(deltaTimeSum >= SpeedIntervalRate)
+            {
+                deltaTimeSum = 0;
+                if(velocityNeeded < MaxSpeed)
+                {
+                    velocityNeeded += SpeedInterval;
+                }
+                else
+                {
+                    SceneManager.LoadScene(nextScene);
+                }
+            }
+
+            if (GetComponent<Rigidbody2D>().velocity.magnitude < velocityNeeded)
+            {
+                timeUnderReq += Time.deltaTime;
+            }
+
+            if (timeUnderReq >= timeAllowedUnderReq)
+            {
+                SceneManager.LoadScene("Fail");
+            }
+
+
+
+        }
 	}
 }
