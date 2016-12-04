431cfb722f5683833804d0485262fea3f39265e0
swangboy55 <mayansymbol@gmail.com>
2016-12-03 19:36:41 -0500

    the goods

--- Assets/Scripts/PixelFilter.cs
+++ Assets/Scripts/PixelFilter.cs
@@ -1,60 +1,22 @@
 ﻿using UnityEngine;
-using System.Collections;
 
+[ExecuteInEditMode]
+[RequireComponent(typeof(Camera))]
+[AddComponentMenu("Image Effects/Pixelization")]
 public class PixelFilter : MonoBehaviour
 {
-
-    //how chunky to make the screen
     public float pixelSize = 4;
-    public FilterMode filterMode = FilterMode.Point;
-    public Camera[] otherCameras;
-    private Material mat;
-    Texture2D tex;
 
-    void Start()
+
+    void OnRenderImage(RenderTexture source, RenderTexture destination)
     {
-        (GetComponent<Camera>()).pixelRect = new Rect(0, 0, (int)(Screen.width / pixelSize), (int)(Screen.height / pixelSize));
-        for (int i = 0; i < otherCameras.Length; i++)
-            otherCameras[i].pixelRect = new Rect(0, 0, (int)(Screen.width / pixelSize), (int)(Screen.height / pixelSize));
-    }
+        float w = (float)source.width / pixelSize;
+        float h = (float)source.height / pixelSize;
 
-    //Draw pixelfilter texture to the screen
-    void OnGUI()
-    {
-        if (Event.current.type == EventType.Repaint)
-            Graphics.DrawTexture(new Rect(0, 0, Screen.width, Screen.height), tex);
-    }
-
-
-    void OnPostRender()
-    {
-        if (!mat)
-        {
-            Shader s = Shader.Find("Hidden/SetAlpha");
-
-            mat = new Material(s);
-        }
-        // Draw a quad over the whole screen with the above shader
-        GL.PushMatrix();
-        GL.LoadOrtho();
-        for (var i = 0; i < mat.passCount; ++i)
-        {
-            mat.SetPass(i);
-            GL.Begin(GL.QUADS);
-            GL.Vertex3(0, 0, 0.1f);
-            GL.Vertex3(1, 0, 0.1f);
-            GL.Vertex3(1, 1, 0.1f);
-            GL.Vertex3(0, 1, 0.1f);
-            GL.End();
-        }
-        GL.PopMatrix();
-
-
-        DestroyImmediate(tex);
-
-        tex = new Texture2D(Mathf.FloorToInt(GetComponent<Camera>().pixelWidth), Mathf.FloorToInt(GetComponent<Camera>().pixelHeight));
-        tex.filterMode = filterMode;
-        tex.ReadPixels(new Rect(0, 0, GetComponent<Camera>().pixelWidth, GetComponent<Camera>().pixelHeight), 0, 0);
-        tex.Apply();
+        var lowRez = RenderTexture.GetTemporary((int)w, (int)h);
+        lowRez.filterMode = FilterMode.Point;
+        Graphics.Blit(source, lowRez);
+        Graphics.Blit(lowRez, destination);
+        RenderTexture.ReleaseTemporary(lowRez);
     }
 }
